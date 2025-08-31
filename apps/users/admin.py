d3262965cdc63adcab39febd9de8a97c3b93303c
stevenrego <stevenrego@hotmail.com>
2025-08-31 16:29:39 +0300

    Create admin.py

---
+++ apps/users/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@admin.register(User) class UserAdmin(admin.ModelAdmin): list_display = ('id', 'email', 'first_name', 'last_name', 'role', 'is_staff') search_fields = ('email', 'first_name', 'last_name')_
