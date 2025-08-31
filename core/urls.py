97cb48fea778cb405ead83cd4a1f89a6875a5aa9
stevenrego <stevenrego@hotmail.com>
2025-08-31 16:19:13 +0300

    Create urls.py

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/health/', include('apps.common.urls')),
+    path('api/auth/', include('apps.users.urls_auth')),
+    path('api/users/', include('apps.users.urls_users')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
+]
