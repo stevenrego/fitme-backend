cbe04d0510ba77c8af7e8c55717a78f985a7daaa
stevenrego <stevenrego@hotmail.com>
2025-09-02 15:35:34 +0300

    Update serializers.py

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,18 +1,25 @@
-from django.contrib.auth import get_user_model from rest_framework import serializers_
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer): class Meta: model = User fields = ['id', 'email', 'first_name', 'last_name', 'role']
 
-class RegisterSerializer(serializers.ModelSerializer): password = serializers.CharField(write_only=True, min_length=8)
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
 
-class Meta: model = User fields = ['email', 'password', 'first_name', 'last_name']
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "first_name", "last_name")
 
-def create(self, validated_data):
-    email = validated_data['email']
-    password = validated_data.pop('password')
-    user = User(**validated_data)
-    user.username = email
-    user.set_password(password)
-    user.save()
-    return user**_
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserMeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name")
